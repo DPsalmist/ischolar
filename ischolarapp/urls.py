e66f0eeebe619a77f37bc30c2d085b14dfdc07c7
Dpsalmist <sdamilare420@gmail.com>
2022-10-15 17:43:59 +0100

    updated home page

--- ischolarapp/urls.py
+++ ischolarapp/urls.py
@@ -7,5 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('school.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
